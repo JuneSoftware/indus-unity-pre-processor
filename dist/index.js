b7da7612bb22a22e2aa557de5fd0552f443cf045
Valiant John <84314269+ValiantJohnCh@users.noreply.github.com>
2022-07-15 01:02:42 +0530

    updated script

--- dist/index.js
+++ dist/index.js
@@ -40,7 +40,7 @@ function run() {
             let item = { platform, subPlatform, modules };
             jsonObject.push(item);
         }
-        core.setOutput('selectedTarget', jsonObject);
+        core.setOutput('selectedTarget', JSON.stringify(jsonObject));
     }
     catch (error) {
         if (error instanceof Error)
