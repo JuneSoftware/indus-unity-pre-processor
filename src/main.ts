ad5a5a31b6b876fa7ebf7382b8be29e4205aa4de
arjun-gaonkar <arjun.gaonkar@supergaming.com>
2024-10-09 11:56:07 +0530

    feat: added playstore and build target five in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ const Windows = "Windows";
 const WindowsServer = "Windows Server";
 const Linux = "Linux";
 const LinuxServer = "Linux Server";
+const PlayStore="Play Store";
 const DefaultSlackObject = '{"Public":"SLACK_WEBHOOK","Private":"SLACK_WEBHOOK_2"}';
 const DefaultEvironmentDataObject = '{"Development":{"GCPKey":"SERVICE_ACCOUNT_KEY_DEV","GCPURL":"GCP_BUILD_URL_PREFIX_DEV","GCPURLPrefix":"indus-builds"},"Staging":{"GCPKey":"GCP_BUILD_URL_PREFIX_STAGING","GCPURL":"SERVICE_ACCOUNT_KEY_STAGING","GCPURLPrefix":"indus-builds-stage"},"Release":{"GCPKey":"GCP_BUILD_URL_PREFIX_STAGING","GCPURL":"SERVICE_ACCOUNT_KEY_STAGING","GCPURLPrefix":"indus-builds-stage"},"Production":{"GCPKey":"GCP_BUILD_URL_PREFIX_PROD","GCPURL":"SERVICE_ACCOUNT_KEY_PROD","GCPURLPrefix":"indus-builds-prod"}}';
 const DefaultBuildConfigDataObject = '{"Default":"Assets/Indus/Platform/Build/Configurations/Config.Build.Default.asset"}';
@@ -19,6 +20,7 @@ async function run(): Promise<void> {
     let buildTargetTwo = core.getInput('buildTargetTwo');
     let buildTargetThree = core.getInput('buildTargetThree');
     let buildTargetFour = core.getInput('buildTargetFour');
+    let buildTargetFive = core.getInput('buildTargetFive');
     let slackChannel = core.getInput('slackChannel');
     let slackData = core.getInput('slackData');
     let buildNumberStepSize = core.getInput('buildNumberStepSize');
@@ -46,6 +48,10 @@ async function run(): Promise<void> {
 
     if (buildTargetFour == '') {
       buildTargetFour = 'None'
+    }
+
+    if (buildTargetFive == '') {
+      buildTargetFive = 'None'
     }
 
     if (slackChannel == '') {
@@ -90,6 +96,10 @@ async function run(): Promise<void> {
     if (item != null)
       jsonObject.push(item);
 
+    item = getMatrixItem(buildTargetFive, buildEnvironment, slackData, slackChannel, evironmentData, buildConfig, buildConfigData);
+    if (item != null)
+      jsonObject.push(item);
+
     const incrementedBuildNumber = await incrementBuildNumber(Number.parseInt(buildNumberStepSize));
     const buildNumber = overrideBuildNumber === 'true' ? customBuildNumber : incrementedBuildNumber;
 
@@ -123,6 +133,7 @@ function getMatrixItem(platformName: string, buildEnvironment: string, slackData
 function getPlatform(platformName: string): string {
   switch (platformName) {
     case Android:
+    case PlayStore:
       {
         return "Android";
       }
@@ -176,6 +187,11 @@ function getCustomPlatformName(platformName: string): string {
       {
         return "LinuxServer64";
       }
+
+    case PlayStore:
+      {
+        return "PlayStore";
+      }
   }
   return "Android";
 }
@@ -206,6 +222,10 @@ function getModules(platformName: string): string {
       {
         return "linux-il2cpp, linux-server";
       }
+    case PlayStore:
+      {
+        return "android-play-store";
+      }
   }
   return "android";
 }
@@ -213,6 +233,7 @@ function getModules(platformName: string): string {
 function getSubPlatformServer(platformName: string): string {
   switch (platformName) {
     case Android:
+    case PlayStore:
       {
         return "Player";
       }
